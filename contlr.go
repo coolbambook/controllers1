0dfbcb78747057625f7c478b2b20dbd44d1e9815
coolbambook <sydykdana10@gmail.com>
2024-10-14 21:45:18 +0600

    Add files via upload

---
+++ contlr.go
@@ -0,0 +1,44 @@
+package controllers
+
+import (
+	"net/http"
+)
+
+type Static struct {
+	Template Template
+}
+
+func (static Static) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	static.Template.Execute(w, nil)
+}
+
+func StaticHandler(tpl Template) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   string
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer:   "Yes! We offer a free trial for 30 days on any paid plans.",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer:   "We have support staff answering emails 24/7, though response"},
+		{
+
+			Question: "How do I contact support?",
+			Answer:   `Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>`,
+		},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
+	}
+}
---
+++ users.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"net/http"
+)
+
+type Users struct {
+	Templates struct {
+		New Template
+	}
+}
+
+func (u Users) New(w http.ResponseWriter, r *http.Request) {
+	u.Templates.New.Execute(w, nil)
+}
